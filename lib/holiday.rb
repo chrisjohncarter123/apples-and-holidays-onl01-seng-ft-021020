0d0bd47992d757e262b724cf8ab00ad845986ef5
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-21 07:00:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -90,7 +90,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |key, value|
     value.each_with_index do |value2, index|
       value2.each_with_index do |value3, index|
-        puts value3
+        #puts value3
       end
       
     end
