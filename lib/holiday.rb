99cbb1f0a93240ef4e5c1a4148d951b26f469f73
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-21 06:47:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{key.capitalize}:"
     
     value.each do |key2, value2|
-      puts "#{key.capitalize}:"
+      puts "  #{key.capitalize},"
     end
   end
 
