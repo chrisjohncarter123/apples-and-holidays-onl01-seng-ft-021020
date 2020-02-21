36264710701165fd6ec748bc7ca2840e6ae3c267
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-21 06:45:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,8 +61,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  holiday_hash.each do |season, key|
-    puts season
+  holiday_hash.each do |key, value|
+    puts key
   end
 
 end
