519ff62c7d0c9f8dc5c01b87c9b68613e7b4d935
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-21 06:58:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,6 +87,12 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
+  holiday_hash.each do |key, value|
+    value.each_with_index do |value2, index|
+      puts value2
+    end
+  end
+
 end
 
 
