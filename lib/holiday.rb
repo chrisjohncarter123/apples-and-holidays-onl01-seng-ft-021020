65ab25510f45675e4a71733202d6074fe4503754
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-21 07:15:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -98,6 +98,7 @@ def all_holidays_with_bbq(holiday_hash)
         
         if(supplies.kind_of?(Array))
           puts "supplies: #{supplies}"
+          puts "#{holiday_index}"
           if(supplies.include?("BBQ"))
             result << holiday_index
           end
